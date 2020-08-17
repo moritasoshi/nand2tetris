2951d9c2f68fa0a831f82529c12ec826ed92483c
moritasoshi <soshi.morita@rakus-partners.co.jp>
2020-08-18 00:39:09 +0900

    create mult.hack

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,9 +1,22 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
+  @2
+  M=0       // R2 = 0
+  @i
+  M=1       // i = 1
+(LOOP)
+  @i
+  D=M
+  @1
+  D=D-M     // D = i - R1
+  @END
+  D;JGT     // D > 0ならばENDへ移動
+  @0
+  D=M
+  @2
+  M=M+D     // 結果を代入
+  @i
+  M=M+1
+  @LOOP
+  0;JMP     // LOOPへ移動
+(END)
+  @END
+  0;JMP     // 無限ループ
